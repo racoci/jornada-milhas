688939f03bdfe24277c2256cc6b04d210e674d4a
Lucas Racoci <racoci@motorola.com>
2025-01-16 09:37:56 -0300

    2.1.5 Implementa interceptor e guarda de rotas.
    - Enviar o `token` em requisições através do interceptor;
    - Proteger as rotas da aplicação utilizando a Guarda de Rotas Funcional;
    - Personalizar o `header` da “Jornada Milhas” de acordo com o login da pessoa usuária.

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -32,7 +32,7 @@ export class PerfilComponent implements OnInit{
 
   ngOnInit() {
     this.token = this.tokenService.retornarToken();
-    this.cadastroService.buscarCadastro(this.token).subscribe(cadastro => {
+    this.cadastroService.buscarCadastro().subscribe(cadastro => {
       this.cadastro = cadastro;
       this.nome = cadastro.nome;
       this.carregarFormulario();
@@ -67,7 +67,7 @@ export class PerfilComponent implements OnInit{
       estado: this.form?.value.estado
     }
 
-    this.cadastroService.editarCadastro(dadosAtualizados, this.token).subscribe({
+    this.cadastroService.editarCadastro(dadosAtualizados).subscribe({
       next: () => {
         alert('Cadastro editado com sucesso')
         this.router.navigate(['/']);
